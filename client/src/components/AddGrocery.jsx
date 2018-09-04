2d401eb00e4a2bf99ea5ea958ec0710a079b933d
zaid <zaidhusseini@gmail.com>
2018-09-04 16:47:02 -0700

    Adds grocery list items text input boxes

--- client/src/components/AddGrocery.jsx
+++ client/src/components/AddGrocery.jsx
@@ -3,12 +3,27 @@ import React from 'react';
 class AddGrocery extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
+      description:'',
+      quantity:''
     }
   }
 
+  changeDescription(e){
+    this.setState({description:e.target.value});
+  }
+
+  changeQuantity(e){
+    this.setState({quantity:e.target.value});
+  }
+
+
   render () {
-    return null;
+    return (<div>
+              Description:<input type="text" value={this.state.description} onChange={this.changeDescription.bind(this)}/>
+              Quantity:<input type="text" value={this.state.quantity} onChange={this.changeQuantity.bind(this)}/>
+              <button style={{marginTop:10, width: 100, height: 30}}>Add Item</button>
+            </div>);
   }
 }
 
