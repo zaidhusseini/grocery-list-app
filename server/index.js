14e768b23817fda93070e2655eb0ceaf5bf3951a
zaid <zaidhusseini@gmail.com>
2018-09-04 18:38:55 -0700

    Updates server to include routing

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var app = express();
+var router = require('./router.js');
 
 app.use(express.static(__dirname + '/../client/dist'));
+
+app.use(router);
 
 app.listen(3000, function() {
   console.log('Server started and listening on port 3000');
